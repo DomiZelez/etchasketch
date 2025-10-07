07bfdbfaaa00d7ec4a0f85328030e03489d5736e
domizelez <domi.fon.zeleznik@gmail.com>
2025-10-07 20:58:14 +0200

    created - adjustable grid, pastel hover effect

---
+++ script.js
@@ -0,0 +1,60 @@
+const  container = document.getElementById("container");
+container.classList.add("container");
+
+const settingButton = document.getElementById("settingButton");
+settingButton.classList.add("settingButton");
+
+function colorRandomizer() {
+    let colorNumber = Math.floor(Math.random() * 5);
+    let pastelColor;
+    switch(colorNumber) {
+        case 0: return pastelColor = "#DABBE0";
+        case 1: return pastelColor = "#EEBFD1";
+        case 2: return pastelColor = "#ECAAC2";
+        case 3: return pastelColor = "#BED1E3";
+        case 4: return pastelColor = "#93AECA";
+    };
+};
+
+let resolution = 16;
+
+const createRow = function (number) {
+    for (let i = 0; i < number; i++)
+    {
+let div = document.createElement("div");
+div.classList.add("div")
+div.style.display = "flex";
+div.style.width = (960/resolution) - 2 + "px";
+div.style.height = (960/resolution) - 2 + "px";
+div.style.backgroundColor = "white";
+div.style.borderStyle = "solid";
+div.style.borderColor = "black";
+div.style.borderWidth = "1px";
+
+div.addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = colorRandomizer();
+})
+container.appendChild(div);
+    }
+}
+
+const createGrid = function (number) {
+    container.replaceChildren();
+    for (let i = 0; i < number; i++)
+    {
+        createRow(resolution);
+    }
+}
+
+createGrid(resolution);
+
+function askUser() {
+    resolution = parseInt(prompt("How many squares per width?"), 10);
+    while (resolution > 100) {
+        resolution = parseInt(prompt("How many squares per width?"), 10);
+    };
+    console.log(resolution);
+    createGrid(resolution);
+}
+
+settingButton.addEventListener("click", askUser);
